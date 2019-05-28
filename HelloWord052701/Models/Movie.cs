794254c81d6aadaa2076c0ef16a501aa7f3867db
Oliver.Lin <Oliver.Lin@avepoint.com>
2019-05-28 13:22:00 +0800

    [Movie-001]Modify:support edit view delete search feature

--- HelloWord052701/Models/Movie.cs
+++ HelloWord052701/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,21 @@ namespace HelloWord052701.Models
     public class Movie
     {
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Range(1, 100), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+
+        [Range(1, 10)]
+        public string Rating { get; set; }
+        
     }
 }
